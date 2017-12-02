0bb9fecdb1dfab7506642106b33dc96c928182cf
Nate Liu <jinliangliu@163.com>
2017-12-02 23:58:34 +0800

    use component to abstract likebutton

--- src/index.js
+++ src/index.js
@@ -4,41 +4,55 @@ const createDOMFromString = (domString) => {
     return div
 }
 
-class LikeButton {
-    constructor() {
-        this.state = {isLiked:false};
+const mount = (componet,wrapper) => {
+    wrapper.appendChild(componet._renderDOM());
+    componet.onStateChange = (oldEl,newEl) => {
+        wrapper.insertBefore(newEl,oldEl);
+        wrapper.removeChild(oldEl);
     }
+}
 
+class Component {
     setState(state){
         const oldEl = this.el;
         this.state = state;
-        this.el = this.render()
+        this.el = this._renderDOM()
         if(this.onStateChange) {
             this.onStateChange(oldEl,this.el);
         }
     }
 
-    changeLikeText = () => {
+    _renderDOM() {
+        this.el = createDOMFromString(this.render());
+        if(this.onClick) {
+            this.el.addEventListener('click',this.onClick.bind(this),false);
+        }
+        return this.el;
+    }
+
+}
+
+class LikeButton extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {isLiked:false};
+    }
+   
+    onClick = () => {
         this.setState(
             {isLiked:!this.state.isLiked}
         );
     }
 
     render() {
-        this.el = createDOMFromString(`
+        return `
         <button class="likeButton">
             <span class="likeText">${this.state.isLiked ? 'Unlike' : 'Like'}</span>
         </button>
-        `);
-        this.el.addEventListener('click',this.changeLikeText.bind(this),false)
-        return this.el;
+        `;
     }
 }
 
 const likeArea = document.querySelector('.likeArea');
 const btn = new LikeButton();
-likeArea.appendChild(btn.render());
-btn.onStateChange = (oldEl,newEl) => {
-    likeArea.insertBefore(newEl,oldEl);
-    likeArea.removeChild(oldEl);
-}
+mount(btn,likeArea);
