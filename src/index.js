cf90c924dbd14c3aba38a47a234b783d40efcc4a
Nate Liu <jinliangliu@163.com>
2017-12-02 22:27:46 +0800

    js perform like button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('init project!');
+const btn = document.querySelector('.likeButton');
+const btnText = btn.querySelector('.likeText');
+let isLiked = false;
+btn.addEventListener('click',() => {
+    isLiked = !isLiked;
+    if(isLiked) {
+        btnText.innerHTML = 'Unlike';
+    } else {
+        btnText.innerHTML = 'Like';
+    }
+}, false);
